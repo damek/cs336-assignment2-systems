95bc83f93f5c765c20f76d426d9af991072bf7a7
damek <damek@users.noreply.github.com>
2025-07-27 16:18:57 -0400

    adding loss function to benchmarking.

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -1,16 +1,24 @@
 import argparse
 import cs336_basics.model as models
+import cs336_basics.nn_utils as nn_utils
 import torch 
+import timeit
 
 
 p = argparse.ArgumentParser()
-p.add_argument("--num_layers", type=float, required=True)
-p.add_argument("--num_heads", type=float, required=True)
-p.add_argument("--context_length", type=float, required=True)
-p.add_argument("--d_model", type=float, required=True)
-p.add_argument("--d_ff", type=float, required=True)
-p.add_argument("--vocab_size", type=float, required=True)
+p.add_argument("--num_layers", type=int, required=True)
+p.add_argument("--num_heads", type=int, required=True)
+p.add_argument("--context_length", type=int, required=True)
+p.add_argument("--d_model", type=int, required=True)
+p.add_argument("--d_ff", type=int, required=True)
+p.add_argument("--vocab_size", type=int, required=True)
 p.add_argument("--rope_theta", type=float, required=True)
+p.add_argument("--num_warmup", type=int, default=5)
+p.add_argument("--num_benchmark", type=int, default=10)
+p.add_argument("--only_forward", action='store_true')
+p.add_argument("--batch_size", type=int, default=4)
+
+
 
 args = p.parse_args()
 
@@ -27,5 +35,39 @@ model = models.BasicsTransformerLM(
 
 # Generate random data
 # Int[Tensor, " ... sequence_length"]
-batch_size = 32
-random_data = torch.randint(low = 0, high = 10000, size = args.sequence_length)
+batch_size = 4
+random_input = torch.randint(low = 0, high = args.vocab_size, size = (batch_size, args.context_length))
+random_target = torch.randint(low = 0, high = args.vocab_size, size = (batch_size, args.context_length))
+
+def loss_fn():
+    return nn_utils.cross_entropy( model(random_input), random_target )
+# Do warmup steps.
+avg_time_warmup = 0
+print("Starting Warmup")
+for i in range(args.num_warmup):
+    print(f"Iteration {i}/{args.num_warmup}")
+    start_time = timeit.default_timer()
+    model.zero_grad()
+    loss = loss_fn()
+    loss.backward()
+    end_time = timeit.default_timer()
+    avg_time_warmup += end_time - start_time
+
+print("Avg time of warmup forward + backward", avg_time_warmup/args.num_warmup)
+
+avg_time_benchmark = 0
+
+for i in range(args.num_benchmark):
+    print(f"Iteration {i}/{args.num_benchmark}")
+    start_time = timeit.default_timer()
+    if args.only_forward: 
+        with torch.no_grad(): 
+            loss = loss_fn()
+    else:
+        model.zero_grad()
+        loss = loss_fn()
+        loss.backward()
+    end_time = timeit.default_timer()
+    avg_time_benchmark += end_time - start_time
+
+print("Avg time of benchmark", avg_time_benchmark/args.num_benchmark)
