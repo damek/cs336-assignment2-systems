c107d4a4a2274c0d48bcad56a5779424c3924a05
damek <damek@users.noreply.github.com>
2025-08-06 01:31:39 -0400

    update benchmarking script to support mixed precision with bfloat16 and adjust random input tensor dtype for consistency

---
+++ cs336_systems/benchmarking_scripts/benchmark_bfloat16.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+OUT_CSV="../outputs/csv/$(date +%F)_table1.1.2.csv"
+mkdir -p "$(dirname "$OUT_CSV")"
+
+sizes=(small medium large xl "2.7B")
+d_models=(768 1024 1280 1600 2560)
+d_ffs=(3072 4096 5120 6400 10240)
+layers=(12 24 36 48 32)
+heads=(12 16 20 25 32)
+
+contexts=(128 256 512 1024)
+
+bfloat =( "on:5" "off:0" )
+
+COMMON_STATIC="--vocab_size 10000 --rope_theta 10000 --batch_size 4 \
+               --num_benchmark 10 --output_csv ${OUT_CSV} --num_warmup 5"
+
+echo "timestamp,num_layers,num_heads,d_model,d_ff,context_length,batch_size,only_forward,mean_s,std_s" > "${OUT_CSV}"
+
+for i in "${!sizes[@]}"; do
+  SIZE=${sizes[$i]}
+  echo; echo "========== ${SIZE^^} MODEL =========="
+
+  for ctx in "${contexts[@]}"; do
+    for fwd_only in "no" "yes"; do
+      [[ "$fwd_only" == "yes" ]] && extra_fwd="--only_forward" || extra_fwd=""
+
+      for bfloat16 in "no" "yes"; do
+        [[ "$bloat16" == "yes" ]] && extra_bfloat16="--only_forward" || extra_bfloat16=""
+        echo "--- ctx=${ctx}  forward=${fwd_only}  bfloat16=${bfloat16}"
+
+        python benchmarking_script.py \
+            --num_layers  "${layers[$i]}"  \
+            --num_heads   "${heads[$i]}"   \
+            --d_model     "${d_models[$i]}"\
+            --d_ff        "${d_ffs[$i]}"   \
+            --context_length "${ctx}"      \
+            ${bfloat16}                    \
+            ${extra_fwd}                   \
+            ${COMMON_STATIC}
+      done
+    done
+  done
+done
+
+echo; echo "All runs finished — results in ${OUT_CSV}"
