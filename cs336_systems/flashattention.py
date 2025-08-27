f20e255291404b6aaf494821399822b822b3f0a9
damek <damek@users.noreply.github.com>
2025-08-27 13:15:01 -0400

    correct einsum operation in FlashAttentionPytorch's backward method to ensure proper gradient dimensionality

--- cs336_systems/flashattention.py
+++ cs336_systems/flashattention.py
@@ -213,7 +213,7 @@ class FlashAttentionPytorch(torch.autograd.Function):
         S = einsum(Q, K, "... i d, ... j d -> ... i j")/ctx.sqrt_d
         P = torch.exp(S - L.unsqueeze(-1))
         dV = einsum(P, dO, "... i j, ... i d -> ... j d")
-        dP = einsum(dO, V, "... i j, ... j d -> ... i j")
+        dP = einsum(dO, V, "... i d, ... j d -> ... i j")
         dS = P * (dP - D)
         dQ = einsum(dS, K, "... a b, ... b c-> ... a c")/ctx.sqrt_d
         dK = einsum(dS, Q, "... a b, ... a d -> ... b d")/ctx.sqrt_d
