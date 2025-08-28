1823c826bbe414a53a72b2827b802075fe82efa5
damek <damek@users.noreply.github.com>
2025-08-28 11:32:38 -0400

    update distributed_communication_single_node to use 'nccl' for process group initialization, optimizing GPU-based communication and ensuring data is allocated on the correct device

--- cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
+++ cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
@@ -6,11 +6,11 @@ import torch.multiprocessing as mp
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29501"
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def distributed_demo(rank, world_size):
     setup(rank, world_size)
-    data = torch.randint(0, 10, (3,))
+    data = torch.randint(0, 10, (3,), device=f"cuda:{rank}")
     print(f"rank {rank} data (before all-reduce): {data}")
     dist.all_reduce(data, async_op=False)
     print(f"rank {rank} data (after all-reduce): {data}")
