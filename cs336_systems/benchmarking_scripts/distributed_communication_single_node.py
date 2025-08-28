fea66e3090bf73c67abf413b6e509f37b608d4dc
damek <damek@users.noreply.github.com>
2025-08-28 11:06:40 -0400

    update distributed_communication_single_node to use 'nccl' for process group initialization, enhancing compatibility with GPU-based communication

--- cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
+++ cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
@@ -6,7 +6,7 @@ import torch.multiprocessing as mp
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29501"
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def distributed_demo(rank, world_size):
     setup(rank, world_size)
