a66968bbbb1736f37c072984af38a8898ac4af88
damek <damek@users.noreply.github.com>
2025-08-28 11:53:29 -0400

    enhance distributed_communication_single_node by adding tensor size parameter and timing measurements for improved performance analysis during all-reduce operations

--- cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
+++ cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
@@ -9,21 +9,31 @@ def setup(rank, world_size):
     os.environ["MASTER_PORT"] = "29501"
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
-def distributed_demo(rank, world_size):
+def distributed_demo(rank, world_size, tensor_size_mb):
     setup(rank, world_size)
+    timing = [None] * world_size
     try: 
-        data = torch.randint(0, 10, (3,), device=f"cuda:{rank}")
+        # float32 tensor of size tensor_size_mb
+        convert_tensor_size = tensor_size_mb * 1024 * 1024 / 4
+        data = torch.randn(convert_tensor_size, device=f"cuda:{rank}", dtype=torch.float32)
+        # print the size of the tensor
+        print(f"rank {rank} tensor size: {data.size()}")
         print(f"rank {rank} data (before all-reduce): {data}")
         start_time = time.perf_counter()
         dist.all_reduce(data, async_op=False)
-        end_time = time.perf_counter()
         # synchronize all processes
         torch.cuda.synchronize()
+        end_time = time.perf_counter()
+        timing[rank] = end_time - start_time
+        dist.all_gather(data, data)
         print(f"rank {rank} data (after all-reduce): {data}")
         print(f"rank {rank} time taken: {end_time - start_time} seconds")
+        # print the minimum time taken
+        print(f"rank {rank} minimum time taken: {min(timing)} seconds")
     finally:
         dist.destroy_process_group()
 
 if __name__ == "__main__":
     world_size = 4
-    mp.spawn(fn=distributed_demo, args=(world_size, ), nprocs=world_size, join=True)
+    tensor_size_mb = 1024
+    mp.spawn(fn=distributed_demo, args=(world_size, tensor_size_mb, ), nprocs=world_size, join=True)
