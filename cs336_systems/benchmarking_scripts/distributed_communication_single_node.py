4f12738af3fa573c82782124f05e6ba0ee45b1ab
damek <damek@users.noreply.github.com>
2025-08-28 11:56:48 -0400

    refactor distributed_communication_single_node to add num_iterations parameter for repeated all-reduce timing, improving performance measurement accuracy

--- cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
+++ cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
@@ -9,7 +9,7 @@ def setup(rank, world_size):
     os.environ["MASTER_PORT"] = "29501"
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
-def distributed_demo(rank, world_size, tensor_size_mb):
+def distributed_demo(rank, world_size, tensor_size_mb, num_iterations):
     setup(rank, world_size)
     # timing = [None] * world_size
     try: 
@@ -18,15 +18,19 @@ def distributed_demo(rank, world_size, tensor_size_mb):
         data = torch.randn(convert_tensor_size, device=f"cuda:{rank}", dtype=torch.float32)
         # print the size of the tensor
         print(f"rank {rank} tensor size: {data.size()}")
+        total_time = 0
         # print(f"rank {rank} data (before all-reduce): {data}")
-        start_time = time.perf_counter()
-        dist.all_reduce(data, async_op=False)
-        # synchronize all processes
-        torch.cuda.synchronize()
-        end_time = time.perf_counter()
+        for i in range(num_iterations):
+            start_time = time.perf_counter()
+            dist.all_reduce(data, async_op=False)
+            # synchronize all processes
+            torch.cuda.synchronize()
+            end_time = time.perf_counter()
+            total_time += end_time - start_time
+        total_time /= num_iterations
         # timing[rank] = end_time - start_time
         # dist.all_gather(data, data)
-        print(f"rank {rank} time taken: {end_time - start_time} seconds")
+        print(f"rank {rank} time taken: {total_time} seconds")
         # print the minimum time taken
         # print(f"rank {rank} minimum time taken: {min(timing)} seconds")
     finally:
@@ -35,4 +39,5 @@ def distributed_demo(rank, world_size, tensor_size_mb):
 if __name__ == "__main__":
     world_size = 4
     tensor_size_mb = 1
-    mp.spawn(fn=distributed_demo, args=(world_size, tensor_size_mb), nprocs=world_size, join=True)
+    num_iterations = 10
+    mp.spawn(fn=distributed_demo, args=(world_size, tensor_size_mb, num_iterations), nprocs=world_size, join=True)
