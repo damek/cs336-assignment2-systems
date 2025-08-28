1018e155e07eb038a8df4533f424acb6cb07197c
damek <damek@users.noreply.github.com>
2025-08-28 12:20:59 -0400

    enhance distributed_demo in distributed_communication_single_node to iterate over additional world sizes and print world size and MB for improved benchmarking visibility

--- cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
+++ cs336_systems/benchmarking_scripts/distributed_communication_single_node.py
@@ -12,11 +12,9 @@ def setup(rank, world_size):
 
 def distributed_demo(rank, world_size, MB, num_iterations, num_warmup_iterations=5):
     setup(rank, world_size)
-    # timing = [None] * world_size
     try: 
         convert_tensor_size = int(MB * 1024 * 1024 / 4)
         data = torch.randn(convert_tensor_size, device=f"cuda:{rank}", dtype=torch.float32)
-        print(f"rank {rank} tensor size: {data.size()} MB: {MB}")
         total_time = 0
         for _ in range(num_warmup_iterations):
             dist.all_reduce(data, async_op=False)
@@ -32,19 +30,15 @@ def distributed_demo(rank, world_size, MB, num_iterations, num_warmup_iterations
         max_time = torch.tensor([total_time], device="cuda")
         dist.all_reduce(max_time, op=dist.ReduceOp.MAX)
         torch.cuda.synchronize()
-        # timing[rank] = end_time - start_time
-        # dist.all_gather(data, data)
-        print(f"rank {rank} time taken: {total_time} seconds")
-        # print the minimum time taken
-        # print(f"rank {rank} minimum time taken: {min(timing)} seconds")
         if rank == 0:
-            print(f"world_size: {world_size} MB: {MB} max time taken: {max_time[0]} seconds")
+            print(f"world_size: {world_size}, MB: {MB}, max time taken: {max_time[0]} seconds")
     finally:
         dist.destroy_process_group()
 
 if __name__ == "__main__":
-    for world_size in [2, 4]:
+    for world_size in [2, 4, 8, 16]:
         for MB in [1, 10, 100, 1000]:
+            print(f"world_size: {world_size} MB: {MB}")
             num_warmup_iterations = 5
             num_iterations = 10
             mp.spawn(fn=distributed_demo, args=(world_size, MB, num_iterations, num_warmup_iterations), nprocs=world_size, join=True)
