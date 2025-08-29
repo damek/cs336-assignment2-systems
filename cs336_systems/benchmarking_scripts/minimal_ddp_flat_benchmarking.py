3d641d6c01c81f55f5023babd11fae040f3f7b12
damek <damek@users.noreply.github.com>
2025-08-29 19:24:52 -0400

    adsf

--- cs336_systems/benchmarking_scripts/minimal_ddp_flat_benchmarking.py
+++ cs336_systems/benchmarking_scripts/minimal_ddp_flat_benchmarking.py
@@ -13,7 +13,7 @@ import time
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "29522"
+    os.environ["MASTER_PORT"] = "29523"
     torch.cuda.set_device(rank)
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     os.environ.setdefault("PYTORCH_CUDA_ALLOC_CONF", "expandable_segments:True")
