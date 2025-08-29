50ea2aee4729323f209661e301e6c184941855dd
damek <damek@users.noreply.github.com>
2025-08-29 12:42:04 -0400

    update MASTER_PORT in naive_ddp_benchmarking.py to ensure consistent port usage for distributed training

--- cs336_systems/benchmarking_scripts/naive_ddp_benchmarking.py
+++ cs336_systems/benchmarking_scripts/naive_ddp_benchmarking.py
@@ -11,7 +11,7 @@ import time
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "29502"
+    os.environ["MASTER_PORT"] = "29500"
     torch.cuda.set_device(rank)
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
