64c004b01fa3ef4d64a2b242155a0e71e34ccf11
damek <damek@users.noreply.github.com>
2025-08-31 15:50:56 -0400

    sadf

--- cs336_systems/ddp_overlap_individual_parameters.py
+++ cs336_systems/ddp_overlap_individual_parameters.py
@@ -15,17 +15,17 @@ class DDPOverlapIndividualParameters(torch.nn.Module):
                 if p.requires_grad:
                     if not p.is_leaf:
                         raise RuntimeError("Parameter is not a leaf tensor")
-                    p.register_post_accumulate_grad_hook(self._make_hook())
+                    p.register_post_accumulate_grad_hook(self._hook())
 
-    def _make_hook(self): 
-        def _hook(param):   
-            if not (dist.is_available() and dist.is_initialized()):
-                return
-            # work = dist.all_reduce(grad, op=dist.ReduceOp.AVG, async_op=True) # gloo doesn't have avg!??!?!
-            work = dist.all_reduce(param.grad, op=dist.ReduceOp.SUM, async_op=True)
-            self._pending.append((param, work))
-            return None
-        return _hook
+
+    def _hook(self, param):   
+        if not (dist.is_available() and dist.is_initialized()):
+            return
+        # work = dist.all_reduce(grad, op=dist.ReduceOp.AVG, async_op=True) # gloo doesn't have avg!??!?!
+        work = dist.all_reduce(param.grad, op=dist.ReduceOp.SUM, async_op=True)
+        self._pending.append((param, work))
+        return None
+
     
     def forward(self, *args, **kwargs):
         return self.module(*args, **kwargs)
