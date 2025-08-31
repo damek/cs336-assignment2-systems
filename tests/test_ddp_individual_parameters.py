a57c80aabfe8cdd44d0097677c10c242dcb94bfa
damek <damek@users.noreply.github.com>
2025-08-31 15:24:55 -0400

    asdf

--- tests/test_ddp_individual_parameters.py
+++ tests/test_ddp_individual_parameters.py
@@ -70,6 +70,7 @@ def _test_DistributedDataParallelIndividualParameters(rank: int, world_size: int
             "no_grad_fixed_param" in ddp_model_param_name or "no_grad_fixed_param" in non_parallel_param_name
         )
         if rank == 0 or is_no_grad_fixed_param:
+            print("Distance between non-parallel and ddp model parameters is: ", torch.norm(non_parallel_model_parameter - ddp_model_parameter))
             assert torch.allclose(non_parallel_model_parameter, ddp_model_parameter)
         else:
             assert not torch.allclose(non_parallel_model_parameter, ddp_model_parameter)
