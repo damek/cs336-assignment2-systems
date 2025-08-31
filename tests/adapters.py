bffc19cc6c5e039a0e61f01d25761dfd711bdb74
damek <damek@users.noreply.github.com>
2025-08-31 14:35:05 -0400

    asdf

--- tests/adapters.py
+++ tests/adapters.py
@@ -56,7 +56,7 @@ def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
         Instance of a DDP class.
     """
     # For example: return DDPIndividualParameters(module)
-    return ddp_overlap_individual_parameters.DDPOverlapIndividualParameters(module)
+    return DDPOverlapIndividualParameters(module)
 
 
 def ddp_individual_parameters_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
