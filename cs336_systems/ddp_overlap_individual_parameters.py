69fce71ccf83dd0649f9b0faae4e558a31a99cf7
damek <damek@users.noreply.github.com>
2025-08-31 16:08:40 -0400

    asdf

--- cs336_systems/ddp_overlap_individual_parameters.py
+++ cs336_systems/ddp_overlap_individual_parameters.py
@@ -21,6 +21,9 @@ class DDPOverlapIndividualParameters(torch.nn.Module):
     def _hook(self, param):   
         if not (dist.is_available() and dist.is_initialized()):
             return
+        if param.grad is None:
+            print(f"param.grad is None for {param.name}")
+            return
         # work = dist.all_reduce(grad, op=dist.ReduceOp.AVG, async_op=True) # gloo doesn't have avg!??!?!
         work = dist.all_reduce(param.grad, op=dist.ReduceOp.SUM, async_op=True)
         self._pending.append((param, work))
@@ -31,7 +34,6 @@ class DDPOverlapIndividualParameters(torch.nn.Module):
     
     def finish_gradient_synchronization(self):
         ws = dist.get_world_size() if (dist.is_available() and dist.is_initialized()) else 1
-        print("ws: ", ws)
         for p, work in self._pending:
             work.wait()
             if ws > 1 and p.grad is not None:
