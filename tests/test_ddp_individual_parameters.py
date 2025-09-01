dcefd87754ae51b2a45fd5c2b7094f766b6422d2
damek <damek@users.noreply.github.com>
2025-08-31 22:32:08 -0400

    sadf

--- tests/test_ddp_individual_parameters.py
+++ tests/test_ddp_individual_parameters.py
@@ -49,7 +49,9 @@ def _test_DistributedDataParallelIndividualParameters(rank: int, world_size: int
 
     # Create a toy model and move it to the proper device.
     # This is our non-parallel baseline.
-    non_parallel_model = model_class().to(device).to(torch.float64)
+    dtype = torch.float32
+    print("Careful, dtype is: ", dtype, "increase precision if you want the paralell and non-parallel to be closer.")
+    non_parallel_model = model_class().to(device).to(dtype)
 
     # Create a DDP model. Note that the weights of this model should
     # match the non-parallel baseline above.
@@ -81,9 +83,9 @@ def _test_DistributedDataParallelIndividualParameters(rank: int, world_size: int
     # Load the dataset from disk, so we can ensure that every rank has the same
     # overall pool of data.
     # Shape: (20, 10)
-    all_x = torch.load(FIXTURES_PATH / "ddp_test_data.pt").to(torch.float64)
+    all_x = torch.load(FIXTURES_PATH / "ddp_test_data.pt").to(dtype)
     # Shape: (20, 5)
-    all_y = torch.load(FIXTURES_PATH / "ddp_test_labels.pt").to(torch.float64)
+    all_y = torch.load(FIXTURES_PATH / "ddp_test_labels.pt").to(dtype)
 
     # Each rank will see only 10 examples (out of the total dataset size of 20)
     assert all_x.size(0) % world_size == 0
