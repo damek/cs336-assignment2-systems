6dd2cec57327ff1a49623ea68e591edbac80aef8
damek <damek@users.noreply.github.com>
2025-09-01 11:29:26 -0400

    asef

--- cs336_systems/benchmarking_scripts/ddp_overlap_individual_parameters_benchmarking_nvtx.sh
+++ cs336_systems/benchmarking_scripts/ddp_overlap_individual_parameters_benchmarking_nvtx.sh
@@ -1,3 +1,6 @@
-nsys profile --python-backtrace=cuda -o ../outputs/nsys/ddp_overlap_individual_parameters_benchmarking_nvtx/naive_ddp uv run ddp_overlap_individual_parameters_benchmarking_nvtx.py --naive_ddp
+OUT_DIR="../outputs/nsys/ddp_overlap_individual_parameters_benchmarking_nvtx"
+mkdir -p "$OUT_DIR"
 
-nsys profile --python-backtrace=cuda -o ../outputs/nsys/ddp_overlap_individual_parameters_benchmarking_nvtx/overlap uv run ddp_overlap_individual_parameters_benchmarking_nvtx.py 
+nsys profile --python-backtrace=cuda -o "$OUT_DIR/naive_ddp" uv run ddp_overlap_individual_parameters_benchmarking_nvtx.py --naive_ddp
+
+nsys profile --python-backtrace=cuda -o "$OUT_DIR/overlap" uv run ddp_overlap_individual_parameters_benchmarking_nvtx.py 
